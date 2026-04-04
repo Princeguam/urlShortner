cf424f3854f709ed29fc0e94c1ca6a3ad3a1d3e1
Okogbule Goodluck <okogbulegoodluck@gmail.com>
2026-04-04 14:36:41 +0100

    add put method to update url and delete route to delete urls. add cache and ratelimitier to the routes

--- src/routes/v1/home/index.ts
+++ src/routes/v1/home/index.ts
@@ -16,21 +16,29 @@ import {
     kUserIdStoreKey,
     kBaseUrl,
     kDefaultLinkExpirationTimeHR,
-} from "../../../constans/index.js";
+    kDefaultRateLimitMaxRequest,
+    kDefaultWindowSeconds,
+} from "../../../constants/index.js";
 import v1AuthMiddleware from "../../../middleware/authMiddleware.js";
 import humps from "humps";
-import { addHours } from "date-fns";
+import { addDays, addHours, isAfter } from "date-fns";
 import { nanoid } from "nanoid";
+import { $Enums } from "../../../../generated/prisma/browser.js";
+import { rateLimiter } from "../../../middleware/rateLimitter.js";
+import { cache } from "../../../middleware/cacheMiddleware.js";
 
 const UrlShortneroute = express.Router();
 
 UrlShortneroute.use(v1AuthMiddleware());
 
+UrlShortneroute.use(
+    rateLimiter(kDefaultRateLimitMaxRequest, kDefaultWindowSeconds),
+);
+
 UrlShortneroute.get(
     "/",
+    cache(60),
     asyncHandler(async (req: Request, res: Response) => {
-        let email = req.store.get(kUserEmailStoreKey);
-        let username = req.store.get(kUsernameStoreKey);
         let userId = req.store.get(kUserIdStoreKey);
 
         let history = await prismaClient.urls.findMany({
@@ -45,6 +53,8 @@ UrlShortneroute.get(
                 ExpiresAt: true,
             },
         });
+
+        console.log("THIS key: ", req.store.get(kUserIdStoreKey));
 
         res.status(200).json(
             systemResponse(
@@ -141,6 +151,108 @@ UrlShortneroute.post(
                 humps.camelizeKeys(pageData),
                 undefined,
             ),
+        );
+    }),
+);
+
+UrlShortneroute.put(
+    "/",
+    asyncHandler(async (req: Request, res: Response) => {
+        let userId = req.store.get(kUserIdStoreKey);
+        let urlId = parseInt(req.body.urlId);
+        let longUrl = req.body.longUrl;
+        let expiration = new Date(req.body?.expiration);
+        let now = new Date();
+
+        if (!expiration) expiration = addDays(now, 1);
+
+        let urlExist = await prismaClient.urls.findFirst({
+            where: {
+                Id: urlId,
+                UserId: userId,
+            },
+            select: {
+                LongUrl: true,
+                ShortUrl: true,
+                IsActive: true,
+                ExpiresAt: true,
+            },
+        });
+
+        if (!urlExist) {
+            let { message, errorCode, statusCode } = HandleServerError(
+                ErrorType.InvalidUrl,
+            );
+            res.status(statusCode).json(
+                systemResponse(false, message, undefined, errorCode),
+            );
+            return;
+        }
+
+        let updatedUrl = await prismaClient.urls.update({
+            where: {
+                Id: urlId,
+                UserId: userId,
+            },
+            data: {
+                LongUrl: longUrl,
+                ExpiresAt: expiration,
+            },
+        });
+
+        let history = await prismaClient.history.create({
+            data: {
+                ChangedById: userId,
+                UrlId: urlId,
+                PreviousLongUrl: urlExist.LongUrl,
+                NewLongUrl: longUrl,
+                Action: $Enums.ChangeAction.UPDATE,
+            },
+            select: {
+                Id: true,
+                PreviousLongUrl: true,
+                NewLongUrl: true,
+                Action: true,
+                Url: {
+                    select: {
+                        ShortUrl: true,
+                        LongUrl: true,
+                        ExpiresAt: true,
+                    },
+                },
+            },
+        });
+
+        console.log(updatedUrl);
+        res.status(200).json(
+            systemResponse(
+                true,
+                kDefaultSuccessMessage,
+                humps.camelizeKeys(history),
+                undefined,
+            ),
+        );
+    }),
+);
+
+UrlShortneroute.delete(
+    "/",
+    asyncHandler(async (req: Request, res: Response) => {
+        let body = req.body;
+        let userId = req.store.get(kUserIdStoreKey);
+
+        console.log("DELETED");
+
+        let deleted = await prismaClient.urls.deleteMany({
+            where: {
+                Id: {
+                    in: body.urlIds,
+                },
+                UserId: userId,
+            },
+        });
+        res.status(200).json(
+            systemResponse(true, kDefaultSuccessMessage, deleted, undefined),
         );
     }),
 );
