a6bd39d279c9d0b909b2c0ce60f8482f5760d8a2
Okogbule Goodluck <okogbulegoodluck@gmail.com>
2026-05-22 11:53:47 +0100

    ➕ add: new values

--- src/constants/values.ts
+++ src/constants/values.ts
@@ -2,3 +2,5 @@ export const kDefaultSaltRounds = 10;
 export const kDefaultLinkExpirationTimeHR = 1;
 export const kDefaultRateLimitMaxRequest = 100;
 export const kDefaultWindowSeconds = 3600;
+export const kDefaultQueryPage = 1;
+export const kDefaultQueryCount = 10;
