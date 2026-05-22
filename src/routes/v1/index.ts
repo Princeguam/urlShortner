6ad459965e0afcb0e01ddca9bbd0c0b6bf555d41
Okogbule Goodluck <okogbulegoodluck@gmail.com>
2026-05-22 11:51:14 +0100

    ➕ add:  new routes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import AuthRoute from "./auth/index.js";
 import UrlShortneroute from "./home/index.js";
-import RedirectRoute from "./url.js";
+import RedirectRoute from "./redirect.js";
+import EmailRoute from "./home/emailVerification.js";
+import AdminRoute from "./admin/index.js";
+import PaymentRoute from "./payment/index.js";
+import ProfileRoute from "./profile/index.js";
 
 const V1Route = express.Router();
 
+V1Route.use("/redirect", RedirectRoute);
 V1Route.use("/auth", AuthRoute);
+V1Route.use("/admin", AdminRoute);
+V1Route.use("/email", EmailRoute);
 V1Route.use("/home", UrlShortneroute);
-V1Route.use("/", RedirectRoute);
+V1Route.use("/payment", PaymentRoute);
+V1Route.use("/profile", ProfileRoute);
 
 export default V1Route;
