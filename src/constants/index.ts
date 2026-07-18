1e501f157d73644c62a33f1e6e9f5bd5f7b94b70
Okogbule Goodluck <okogbulegoodluck@gmail.com>
2026-07-18 22:01:56 +0100

    🛠️ fix: change prisma generated folder location to allow for build inside docker container

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,5 +7,5 @@ export interface SignUpBody {
 export * from "./error.js";
 export * from "./strings.js";
 export * from "./values.js";
-export { $Enums } from "../../generated/prisma/browser.js";
+export { $Enums } from "../generated/prisma/browser.js";
 export * as $Types from "./prisma-types.js";
